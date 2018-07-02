f31278ffb2cd91835ce344004e20debaa0e2ea2b
pvitic <paul.vitic@smava.de>
2018-07-02 16:19:11 +0200

    fix

--- .circleci/deploy_qa.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# deploy_qa.sh <servicename> <version> <green-deployment.yaml>
-# Deployment name should be <service>-<version>
-
-kubectl describe service webserver > webserver.txt
-cat webserver.txt
